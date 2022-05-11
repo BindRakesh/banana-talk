266fecce81dbd8d283789702e7e9096619c9455e
BindRakesh <rks.bind@gmail.com>
2022-05-11 17:44:35 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("script is running")
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#text-input");
+var textOutput = document.querySelector("#text-output");
+
+function clickHandler(){
+    textOutput.innerHTML = "output: " + textInput.value;
+}
+
+btnTranslate.addEventListener("click", clickHandler)
